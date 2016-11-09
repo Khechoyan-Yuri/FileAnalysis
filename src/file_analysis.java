47fd455df93604649cd3cf02bcb613cc1e858776
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2016-11-08 23:15:01 -0600

    Greatly modularized the program by turning chunks of code into methods.

--- src/file_analysis.java
+++ src/file_analysis.java
@@ -5,12 +5,73 @@ import java.util.ArrayList;
 public class file_analysis {
 
 	public static void main(String[] args) throws IOException {
-		int num_of_lines = 0;
 		String inputFileName;
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please type in the name of the input file: ");
 		inputFileName = input.nextLine();
 		input.close();
+		
+		String stringArray[] = ReadInputFile(inputFileName);
+		
+		ArrayList<String> stringArrayList = new ArrayList<String>();
+		
+		for(int i=0; i<stringArray.length; i++) {
+			String [] wordArray = stringArray[i].toLowerCase().split("[,;.\\s]+");
+			for(int j=0; j<wordArray.length; j++) {
+				stringArrayList.add(wordArray[j]);
+			}
+		}
+		
+		int uppercaseCount = countUpperCaseLetters(stringArray);
+		
+		String doubleArray[][] = new String [stringArrayList.size()][2];
+		int frequencyCount = 0;
+		boolean ifDoesNotExist = true;
+		int place=0;
+		
+		for(int i=0; i<stringArrayList.size(); i++) {
+			for(int j=0; j<doubleArray[i].length; j++) {
+				if(doubleArray[i][0] != null && doubleArray[i][0].equals(stringArrayList.get(j))) {
+					ifDoesNotExist = false;
+				}
+			}
+			
+			if(ifDoesNotExist) {
+				doubleArray[place][0] = stringArrayList.get(i);
+				place++;
+			}
+			
+			ifDoesNotExist = true;
+			
+		}
+		
+		for(int i=0; i<stringArrayList.size(); i++) {
+			for(int j=0; j<doubleArray.length; j++) {
+				if(doubleArray[i][0].equals(stringArrayList.get(j))) {
+					frequencyCount++;
+				}
+			}
+			doubleArray[i][1] = Integer.toString(frequencyCount++);
+			frequencyCount =0;
+		}
+			String[] trackWord = new String [100];
+			
+		
+		for(int i =0; i<doubleArray.length; i++) {
+			for(int j = 0; j< trackWord.length; j++){
+				if(trackWord[i] != null && trackWord[i].equals(stringArrayList.get(j))) {
+					ifDoesNotExist = false;
+			}
+			
+			}
+			//System.out.println(doubleArray[i][0]+": "+doubleArray[i][1]);
+		}
+		
+	}
+	
+	public static String[] ReadInputFile (String inputFileName) throws IOException {
+		int num_of_lines = 0;
+		
 		File f = new File(inputFileName);
 		Scanner fileCount = new Scanner(f);
 		
@@ -31,92 +92,87 @@ public class file_analysis {
 		}
 		fileInput.close();
 		
-		int wordCount = 0;
-		int sentenceCount = 0;
-		int alphabetCount = 0;
-		int punctuationCount = 0;
+		return stringArray;
+	}
+	
+	public static int countDigits(String [] stringArray){
 		int digitCount = 0;
-		int whitespaceCount = 0;
-		int uppercaseCount = 0;
-		for(i = 0; i<stringArray.length; i++) {
-			String [] wordArray = stringArray[i].split("[, . ; \\s]+");
-			wordCount +=wordArray.length;
-			String [] sentenceArray = stringArray[i].split("[. ; ]+");
-			sentenceCount +=sentenceArray.length;
+		for(int i =0; i<stringArray.length; i++) {
 			for(int j =0; j<stringArray[i].length(); j++) {
-				if(Character.isLetter(stringArray[i].charAt(j))) {
-					alphabetCount++;
-				}
-				else if(stringArray[i].charAt(j) == ',' ||stringArray[i].charAt(j) == ';'
-						|| stringArray[i].charAt(j) == '.') {
-					punctuationCount++;
-				}
-				else if(Character.isDigit(stringArray[i].charAt(j))) {
+				if(Character.isDigit(stringArray[i].charAt(j))) {
 					digitCount++;
 				}
-				else if(stringArray[i].charAt(j) == ' ') {
+			}
+		}
+		return digitCount;
+	}
+	
+	public static int countWords(String [] stringArray) {
+		int wordCount =0;
+		for(int i=0; i<stringArray.length; i++) {
+			String [] wordArray = stringArray[i].split("[, . ; \\s]+");
+			wordCount +=wordArray.length;
+		}
+		return wordCount;
+	}
+	
+	public static int countSentences(String [] stringArray) {
+		int sentenceCount = 0;
+		for(int i=0; i<stringArray.length; i++) {
+			String [] sentenceArray = stringArray[i].split("[. ; ]+");
+			sentenceCount +=sentenceArray.length;
+		}
+		return sentenceCount;
+	}
+	
+	public static int countLetters(String [] stringArray) {
+		int letterCount = 0;
+		for(int i=0; i<stringArray.length; i++) {
+			for(int j=0; j<stringArray[i].length(); j++) {
+				if(Character.isLetter(stringArray[i].charAt(j))) {
+					letterCount++;
+				}
+			}
+		}
+		return letterCount;
+	}
+	
+	public static int countPunctuation(String [] stringArray) {
+		int punctuationCount = 0;
+		for(int i=0; i<stringArray.length; i++) {
+			for(int j=0; j<stringArray[i].length(); j++) {
+				if(stringArray[i].charAt(j) == ',' ||stringArray[i].charAt(j) == ';'
+						|| stringArray[i].charAt(j) == '.') {
+						punctuationCount++;
+				}
+			}
+		}
+		return punctuationCount++;
+	}
+	
+	public static int countWhitespace(String [] stringArray) {
+		int whitespaceCount = 0;
+		for(int i=0; i<stringArray.length; i++) {
+			for(int j=0; j<stringArray[i].length(); j++) {
+				if(stringArray[i].charAt(j) == ' ') {
 					whitespaceCount++;
 				}
-				else if(Character.isUpperCase(stringArray[i].charAt(j))) {
+			}
+		}
+		
+		return whitespaceCount;
+	}
+	
+	public static int countUpperCaseLetters(String [] stringArray) {
+		int uppercaseCount = 0;
+		for(int i = 0; i<stringArray.length; i++) {
+			for(int j =0; j<stringArray[i].length(); j++) {
+				if(((j -1 >=0 && stringArray[i].charAt(j-1) == ' ') || (j==0))  && Character.isUpperCase(stringArray[i].charAt(j))) {
 					uppercaseCount++;
 				}
-				
-			}
-					
-			
-		}
-		
-		ArrayList<String> stringArrayList = new ArrayList<String>();
-		
-		for(i=0; i<stringArray.length; i++) {
-			String [] wordArray = stringArray[i].toLowerCase().split("[,;.\\s]+");
-			for(int j=0; j<wordArray.length; j++) {
-				stringArrayList.add(wordArray[j]);
 			}
 		}
-		
-		String doubleArray[][] = new String [stringArrayList.size()][2];
-		int frequencyCount = 0;
-		boolean ifDoesNotExist = true;
-		int place=0;
-		
-		for(i=0; i<stringArrayList.size(); i++) {
-			for(int j=0; j<doubleArray[i].length; j++) {
-				if(doubleArray[i][0] != null && doubleArray[i][0].equals(stringArrayList.get(j))) {
-					ifDoesNotExist = false;
-				}
-			}
-			
-			if(ifDoesNotExist) {
-				doubleArray[place][0] = stringArrayList.get(i);
-				place++;
-			}
-			
-			ifDoesNotExist = true;
-			
-		}
-		
-		for(i=0; i<stringArrayList.size(); i++) {
-			for(int j=0; j<doubleArray.length; j++) {
-				if(doubleArray[i][0].equals(stringArrayList.get(j))) {
-					frequencyCount++;
-				}
-			}
-			doubleArray[i][1] = Integer.toString(frequencyCount++);
-			frequencyCount =0;
-		}
-			String[] trackWord = new String [100];
-			
-		
-		for(i =0; i<doubleArray.length; i++) {
-			for(int j = 0; j< trackWord.length; j++){
-				if(trackWord[i] != null && trackWord[i].equals(stringArrayList.get(j))) {
-					ifDoesNotExist = false;
-			}
-			
-			}
-			System.out.println(doubleArray[i][0]+": "+doubleArray[i][1]);
-		}
-		
+		System.out.println(uppercaseCount);
+		return uppercaseCount;
 	}
 }
